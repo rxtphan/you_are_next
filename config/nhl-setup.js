b167b9990b5cc39d64b98eb4a457d9ba37cce004
Richard Phan <richard@change.org>
2017-03-05 20:06:39 -0800

    Everything now configurable from nhl-setup.js

---
+++ config/nhl-setup.js
@@ -0,0 +1,32 @@
+module.exports = {
+  positions: [
+    {
+      name: 'Forwards',
+      color: 'panel-primary',
+      stats: [
+        { id: 'forwardGoals', name: 'Goals', max: 5, default: 1 },
+        { id: 'forwardAssists', name: 'Assists', max: 5, default: 1 }
+      ]
+    },
+    {
+      name: "Defensemen",
+      color: 'panel-success',
+      stats: [
+        { id: 'defensemanGoals', name: 'Goals', max: 5, default: 2 },
+        { id: 'defensemanAssists', name: 'Assists', max: 5, default: 2 }
+      ]
+    },
+    {
+      name: "Goalies",
+      color: 'panel-warning',
+      stats: [
+        { id: 'goalieWins', name: 'Wins', max: 5, default: 3 },
+        { id: 'goalieOTLosses', name: 'OT Losses', max: 5, default: 1 },
+        { id: 'goalieShootoutLosses', name: 'Shootout losses', max: 5, default: 1 },
+        { id: 'goalieShutouts', name: 'Shutouts', max: 5, default: 2 },
+        { id: 'goalieGoals', name: 'Goals', max: 10, default: 10 },
+        { id: 'goalieAssists', name: 'Assistsx', max: 10, default: 5 }
+      ]
+    }
+  ]
+}
--- server.js
+++ server.js
@@ -1,18 +1,35 @@
 var express = require("express"),
   exphbs  = require('express-handlebars'),
-  hbs = exphbs.create({ defaultLayout: 'main' }),
   mysql = require('mysql'),
   app  = express(),
   router = express.Router(),
   path = require("path"),
   port = 3000;
 
+var hbs = exphbs.create({
+  defaultLayout: 'main',
+  helpers: {
+    times: function (n, block) {
+      var accum = '';
+      for(var i = 0; i <= n; ++i)
+          accum += block.fn(i);
+      return accum;
+    },
+    if_eq: function(a, b, opts) {
+      if(a == b) {
+        return opts.fn(this);
+      } else {
+        return opts.inverse(this);
+      }
+    }
+  }
+});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-    res.render('nhl');
+    res.render('nhl', require('./config/nhl-setup'));
 });
 
 app.get('/rank', function (req, res) {
